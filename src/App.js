1df3d9968174777bd8056d3b70a52eb4c0e4d25d
MaxMariani <wmaxmariani@gmail.com>
2025-04-23 08:48:01 +0200

    Handling Http States & Feedback with Redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
 import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
@@ -37,6 +41,11 @@ function App() {
       );
     };
 
+    if (isInitial) {
+      isInitial = !isInitial;
+      return;
+    }
+
     sendCartData().catch(
       dispatch(
         uiActions.showNotification({
@@ -49,10 +58,20 @@ function App() {
   }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
