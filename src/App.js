1c11e3790fb65e0eea95c6b7dd239f66296ffbbc
MaxMariani <wmaxmariani@gmail.com>
2025-04-23 08:34:23 +0200

    Handled Http States Feedback

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch(
-      "https://advanced-redux-d9257-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-      { method: "PUT", body: JSON.stringify(cart) }
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending Cart Data...",
+        })
+      );
+      const response = await fetch(
+        "https://advanced-redux-d9257-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success...",
+          message: "Sent Cart Data successfully!",
+        })
+      );
+    };
+
+    sendCartData().catch(
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error...",
+          message: "Sending Cart Data failed!",
+        })
+      )
     );
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
     <Layout>
